697703c38113fee5e177d9703f3d2f9312e26e3b
DavronDilmurodov <dilmurodov553@gmail.com>
2023-03-09 21:07:36 +0500

    First lesson of JS

---
+++ js/main.js
@@ -0,0 +1,39 @@
+var name = prompt("What is your name?");
+
+console.log("Name:", name);
+
+var surname = prompt("What is your surname?");
+
+console.log("Surname:", surname);
+
+var age = prompt("How old are you?");
+
+console.log("Age:", age);
+
+var jobOrStudy = prompt("Working place / Studying place");
+
+console.log("Job / Study:", jobOrStudy);
+
+var gender = prompt("Male / Female");
+
+console.log("Gender:", gender);
+
+var country = prompt("Where are you from");
+
+console.log("Country:", country);
+
+var city = prompt("In which city are you living");
+
+console.log("City:", city);
+
+var religion = prompt("Your religion");
+
+console.log("Religion:", religion);
+
+var level_of_english = prompt("What`s your level of English");
+
+console.log("Level:", level_of_english);
+
+var limit_of_age = confirm("Have you 18?");
+
+console.log("True / False", limit_of_age);
